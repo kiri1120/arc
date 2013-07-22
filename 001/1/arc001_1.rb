19c34fe9599514454c4ee8d4fb9b4aef730b3ed6
kiri1120 <kazuki.s1120@gmail.com>
2013-07-22 17:40:37 +0900

    001 complete

--- 001/1/arc001_1.rb
+++ 001/1/arc001_1.rb
@@ -1,5 +1,14 @@
 #! /usr/bin/env ruby
 # coding: utf-8
 
-data = STDIN.read()
-puts(data)
+data = STDIN.read
+length, answers = data.split("\n")
+
+list = [0,0,0,0]
+
+answers.split('').each do |answer|
+  list[answer.to_i - 1] += 1
+end
+
+puts list.max.to_s + ' ' + list.min.to_s
+
