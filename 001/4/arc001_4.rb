1944d8b702afb0b28aa9c289465dfadde821ca20
kiri1120 <kazuki.s1120@gmail.com>
2013-07-30 12:48:13 +0900

    001_4挫折

--- 001/4/arc001_4.rb
+++ 001/4/arc001_4.rb
@@ -1,10 +1,12 @@
 #!/usr/bin/env ruby
 # coding: utf-8
 
-n = gets.strip.to_i
-start, goal = gets.strip.split(" ").map(&:to_i);
 course_l = []
 course_r = []
+curve = []
+
+n = gets.strip.to_i
+start, goal = gets.strip.split(" ").map(&:to_i);
 (0 .. n).each do |i|
   course_l[i], course_r[i] = gets.strip.split(" ").map(&:to_i);
 end
@@ -12,40 +14,57 @@ end
 course_l[n] = goal
 course_r[n] = goal
 
-ty = 0;
-lva = nil
-rva = nil;
-lwy = 0
-rwy = 0;
-d = 0.0;
+curve[0] = start
+curve[n] = goal
 
-(1 .. n).each do |y|
-  va = (course_l[y] - start).to_f / (y - ty).to_f;
-  if (lva == nil) || (va > lva)
-    lva = va;
-    lwy = y;
-  end
+a = 0
+b = 0
 
-  va = (course_r[y] - start).to_f / (y - ty).to_f;
-  if (rva == nil) || (va < rva)
-    rva = va;
-    rwy = y;
-  end
+begin
+  collision = false
 
-  if lva > rva
-    if y == rwy
-      x = course_l[lwy];
-      y = lwy;
-    else
-      x = course_r[rwy];
-      y = rwy;
+  (1 .. n).each do |x|
+    unless curve[x].nil?
+      next
     end
-    p d += Math::sqrt((x - start) ** 2 + (y - ty) ** 2);
-    start = x
-    ty = y
-    lva = nil; rva = nil;
-  end
-end
 
-d += Math::sqrt((goal - start) ** 2 + (n - ty) ** 2);
-puts d
+    x1 = 0;
+    x2 = n;
+    (x - 1 .. 0).each do |i|
+      unless curve[i].nil?
+        x1 = i
+        break
+      end
+    end
+    (x + 1 .. n).each do |i|
+      unless curve[i].nil?
+        x2 = i
+        break
+      end
+    end
+
+    a = (curve[x2] - curve[x1]).to_f / (x2 - x1).to_f
+    b = (curve[x1]) - a * x1
+
+    y = a * x + b
+    if y < course_l[x]
+      collision = true
+      curve[x] = course_l[x]
+    elsif y > course_r[x]
+      collision = true
+      curve[x] = course_r[x]
+    end
+  end
+end while collision == true
+
+distance = 0.0
+lx = 0
+(1 .. n).each do |x|
+  if curve[x].nil?
+    next
+  end
+
+  distance += Math::sqrt((curve[x] - curve[lx]) ** 2 + (x - lx) ** 2)
+  lx = x
+end
+puts distance
